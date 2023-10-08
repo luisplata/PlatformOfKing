220a16a2648d5ba950dbaee78a4f38c9e0c3cd61
PeryLoth <www.luisplata@gmail.com>
2023-10-08 12:26:31 -0500

    Update build to android

--- Assets/Scripts/Game/PlayerFather.cs
+++ Assets/Scripts/Game/PlayerFather.cs
@@ -1,6 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class PlayerFather : MonoBehaviour
 {
     public InputFacade inputFacade;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("DeadZone"))
+        {
+            //Game Over
+            RestartGame();
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            StartCoroutine(CorroutineToRestartGame());
+        }
+    }
+
+    private IEnumerator CorroutineToRestartGame()
+    {
+        yield return new WaitForSeconds(1f);
+        RestartGame();
+    }
+
+    private static void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
 }
